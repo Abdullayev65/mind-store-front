ef5cfe2a04fa28a9880abb025254e66c2179c7df
Abdullayev65 <abdullayev.65.dev@gmail.com>
2023-06-03 16:03:16 +0500

    65l axios.defaults.baseURL = localhost

--- src/serveice/axios.js
+++ src/serveice/axios.js
@@ -3,7 +3,7 @@ import localStore from "@/helpers/persistaneStorage";
 
 axios.defaults.baseURL = "https://mind-store.jprq.live/api/v1"
 axios.defaults.baseURL = "https://back-end.jprq.live/api/v1"
-// axios.defaults.baseURL = "http://localhost:8765/api/v1"
+axios.defaults.baseURL = "http://localhost:8765/api/v1"
 
 axios.interceptors.request.use(cnfg => {
     const token = localStore.getItem('token')
